93856eb62285afa43ed7e1d0f6cff248e34998ef
Jeranguz <JeremyGD997@hotmail.com>
2024-11-18 09:48:09 -0600

    feat: endpoint to see all reservations

---
+++ ApiHotelesBeach/ApiHotelesBeach/Controllers/ReservasController.cs
@@ -0,0 +1,27 @@
+﻿using ApiHotelesBeach.Data;
+using ApiHotelesBeach.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiHotelesBeach.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReservasController : Controller
+    {
+        private readonly DbContextHotel _context = null;
+
+        public ReservasController(DbContextHotel pContext)
+        {
+            _context = pContext;
+        }
+
+        [HttpGet("Listado")]
+        public List<Reserva> Listado()
+        {
+            List<Reserva> reservas = null;
+            reservas = _context.Reservas.ToList();
+            return reservas;
+        }
+    }
+}
