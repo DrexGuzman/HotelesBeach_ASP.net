6a2b7af0aede5510915408d232fce0444cd2e4d1
ashleyrojas <ashleycr33@gmail.com>
2024-11-18 21:48:03 -0600

    feat: creating a Service for invoices

---
+++ ApiHotelesBeach/ApiHotelesBeach/Services/InvoiceService.cs
@@ -0,0 +1,25 @@
+﻿using PdfSharp.Pdf;
+using MigraDoc.DocumentObjectModel;
+using MigraDoc.Rendering;
+
+namespace ApiHotelesBeach.Services
+{
+    public class InvoiceService
+    {
+
+        public PdfDocument GetInvoice()
+        {
+            // Create a document
+            var document = new Document();
+
+            // Render the document to a PDF file
+            var pdfRenderer = new PdfDocumentRenderer();
+            pdfRenderer.Document = document;
+
+            pdfRenderer.RenderDocument();
+
+            return pdfRenderer.PdfDocument;
+        }
+
+    }
+}
