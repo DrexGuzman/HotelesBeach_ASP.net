ad5514b0385fd8bdbb44a1bb96a6def3b6526baa
DrexGuzman <drexjesusguzman@gmail.com>
2024-11-19 23:19:16 -0600

    Register y consumo de api de paquetes

--- HotelesBeachProyecto/HotelesBeachProyecto/Controllers/PaquetesController.cs
+++ HotelesBeachProyecto/HotelesBeachProyecto/Controllers/PaquetesController.cs
@@ -1,36 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using HotelesBeachProyecto.Models;
 using HotelesBeachProyecto.Data;
+using Newtonsoft.Json;
 
 namespace HotelesBeachProyecto.Controllers
 {
     public class PaquetesController : Controller
     {
-        private readonly DbContextHotel _context;
+        private HotelAPI hotelAPI;
+        private HttpClient client;
+
+        public PaquetesController()
+        {
+                hotelAPI = new HotelAPI();
+                client = hotelAPI.Inicial();
+        }
 
         // GET: Paquetes
-        public ActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var paquetes = new List<Paquete>
-    {
-        new Paquete { Id = 1, Nombre = "Todo Incluido", Costo = 450, Prima = 45, Mensualidades = 24 },
-        new Paquete { Id = 2, Nombre = "Alimentación", Costo = 275, Prima = 35, Mensualidades = 18 },
-        new Paquete { Id = 3, Nombre = "Hospedaje", Costo = 210, Prima = 15, Mensualidades = 12 }
-    };
-            //var paquetes = _context.Paquetes.ToList();
+
+            List<Paquete> paquetes = new List<Paquete>();
+
+            //se utiliza el metodo de la api
+            HttpResponseMessage response = await client.GetAsync("/Paquetes/Listado");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var resultado = response.Content.ReadAsStringAsync().Result;
+
+                paquetes = JsonConvert.DeserializeObject<List<Paquete>>(resultado);
+            }
+
             return View(paquetes);
+
         }
 
         // GET: Paquetes/Reservar/{id}
         public ActionResult Reservar(int id)
         {
-            var paquete = _context.Paquetes.Find(id);
-            if (paquete == null)
-            {
-                return NotFound();
-            }
+            //var paquete = _context.Paquetes.Find(id);
+            //if (paquete == null)
+            //{
+            //    return NotFound();
+            //}
 
-            return View(paquete);
+            return View();
         }
 
         // POST: Paquetes/Reservar
@@ -38,14 +53,14 @@ namespace HotelesBeachProyecto.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Reservar(int id, string nombreCliente)
         {
-            var paquete = _context.Paquetes.Find(id);
-            if (paquete == null)
-            {
-                return NotFound();
-            }
+            //var paquete = _context.Paquetes.Find(id);
+            //if (paquete == null)
+            //{
+            //    return NotFound();
+            //}
 
-            // Aquí se puede implementar la lógica para almacenar la reserva
-            ViewBag.Message = $"¡Reserva realizada con éxito para el paquete {paquete.Nombre} a nombre de {nombreCliente}!";
+            //// Aquí se puede implementar la lógica para almacenar la reserva
+            //ViewBag.Message = $"¡Reserva realizada con éxito para el paquete {paquete.Nombre} a nombre de {nombreCliente}!";
             return RedirectToAction("Index");
         }
 
---
+++ HotelesBeachProyecto/HotelesBeachProyecto/Data/HotelAPI.cs
@@ -0,0 +1,14 @@
+﻿namespace HotelesBeachProyecto.Data
+{
+    public class HotelAPI
+    {
+
+        public HttpClient Inicial() { 
+        
+            var client= new HttpClient();
+            //client.BaseAddress = new Uri(""); cuando este deployado el api en sommee
+            client.BaseAddress = new Uri("https://localhost:7060/");
+            return client;
+        }
+    }
+}
