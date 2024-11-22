307bddad671018f1f985ed1f534ef432ab0cab2b
JoiJaf <jafethrocha11@gmail.com>
2024-11-22 17:08:14 -0600

    Coneccion y reservas

--- HotelesBeachProyecto/HotelesBeachProyecto/Data/HotelAPI.cs
+++ HotelesBeachProyecto/HotelesBeachProyecto/Data/HotelAPI.cs
@@ -6,8 +6,8 @@
         public HttpClient Inicial() { 
         
             var client= new HttpClient();
-            //client.BaseAddress = new Uri(""); cuando este deployado el api en sommee
-            client.BaseAddress = new Uri("https://localhost:7060/");
+            client.BaseAddress = new Uri("http://apihotelbeach.somee.com"); //cuando este deployado el api en sommee
+            //client.BaseAddress = new Uri("https://localhost:7060/");
             return client;
         }
     }
